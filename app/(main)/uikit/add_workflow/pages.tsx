0f95689cab0820ee9f1cf6e52313f8ef85f293b0
vignesh.muralimohan <vignesh.muralimohan@straive.com>
2024-08-14 13:09:09 +0530

    err fix

--- app/(main)/uikit/add_workflow/pages.tsx
+++ app/(main)/uikit/add_workflow/pages.tsx
@@ -5,7 +5,7 @@ import { Dialog } from 'primereact/dialog';
 import { Button } from 'primereact/button';
 import { Steps } from 'primereact/steps';
 import { Toast } from 'primereact/toast';
-import { FloatLabel } from 'primereact/FloatLabel';
+import { FloatLabel } from 'primereact/floatLabel';
 import { InputText } from 'primereact/inputtext';
 import { Accordion, AccordionTab } from 'primereact/accordion';
 import { Badge } from 'primereact/badge';
